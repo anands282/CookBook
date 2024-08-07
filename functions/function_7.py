4a46fe47d471e15799d8a10d6d72e002a9c45b6d
Anand Satheesh <anands282@gmail.com>
2024-08-07 18:47:32 +0530

    updated comments for output

--- functions/function_7.py
+++ functions/function_7.py
@@ -1,3 +1,7 @@
 # attaching informational metadata to functions
 def add(x:int,y:int)->int:
     return x+y
+
+print(add.__annotations__)
+#/Users/anandsatheesh/CookBook/.venv/bin/python /Users/anandsatheesh/CookBook/functions/function_7.py
+#{'x': <class 'int'>, 'y': <class 'int'>, 'return': <class 'int'>}
