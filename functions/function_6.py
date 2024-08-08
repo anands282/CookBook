68863d057a7e9b4633ab17286fce23121ecbce28
Anand Satheesh <anands282@gmail.com>
2024-08-08 18:40:34 +0530

    specific keyword fucntions

---
+++ functions/function_6.py
@@ -0,0 +1,12 @@
+#specify keyword arguments for functions that accept
+#a varying number of positional arguments. For example:
+
+
+def minimum(*values, clip=None):
+    m = min(values)
+    if clip is not None:
+        m = clip if clip > m else m
+    return m
+
+print(minimum(1, 5, 2, -5, 10)) # Returns -5
+print(minimum(1, 5, 2, -5, 10, clip=0)) # Returns 0
