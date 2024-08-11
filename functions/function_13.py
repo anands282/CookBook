6042390fc73e781ae8bd6ebfe28c2d432164c12b
Anand Satheesh <anands282@gmail.com>
2024-08-11 15:19:41 +0530

    added anonymous function variables to store a default value

--- functions/function_13.py
+++ functions/function_13.py
@@ -1,7 +1,6 @@
 #If you need to reduce the number of arguments to a function, you should use func tools.partial().
 # The partial() function allows you to assign fixed values to one or
-#more of the arguments, thus reducing the number of arguments that need to be supplied
-#to subsequent calls
+#more of the arguments, thus reducing the number of arguments that need to be supplied to subsequent calls
 from functools import partial
 
 def spam(a, b, c, d):
