b95d72299b1951e2584ac441b7e7836a5e733d5b
Anand Satheesh <anands282@gmail.com>
2024-08-13 15:04:19 +0530

    updated comments for change in anonymous function variables

--- functions/function_11.py
+++ functions/function_11.py
@@ -1,3 +1,5 @@
+# In the below code the value of x used in the anonymous function will not be set to an initial value it
+# can change based on reassignment
 x = 10
 a = lambda y: x +y
 print(a(10))
